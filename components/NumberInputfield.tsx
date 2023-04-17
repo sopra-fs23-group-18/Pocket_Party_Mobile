b616b25c3c22b71629a343fcd4d89f0519ac197d
Sven Ringger <sven.ringger@hotmail.com>
2023-04-17 16:30:30 +0200

    Add support for typing lobby code #1

---
+++ components/NumberInputfield.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const NumberInputField = () => {
+    const [value, setValue] = useState<number | undefined>(undefined);
+
+    const handleTextChange = (text: string) => {
+        const parsedValue = parseInt(text);
+        if (!isNaN(parsedValue)) {
+            setValue(parsedValue);
+            const length = value?.toString().length;
+            if (length == 6) {//TODO Sven: set right number
+                //TODO Sven: send number to backend for joining lobby
+            }
+        } else {
+            setValue(undefined);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                keyboardType='numeric'
+                placeholder='Enter your code'
+                placeholderTextColor={"#ffffff"}
+                value={value?.toString() ?? ''}
+                onChangeText={handleTextChange}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        height: Dimensions.get('window').height / 18,
+        width: '80%',
+        borderWidth: 1,
+        borderColor: '#ffffff',
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        backgroundColor: '#53A57D',
+        marginVertical: 10,
+        color: "#ffffff"
+    },
+});
+
+
+export default NumberInputField;
--- pages/QRScanner.tsx
+++ pages/QRScanner.tsx
@@ -1,42 +1,51 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, Linking } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+import NumberInputField from '../components/NumberInputfield';
 
-const QRScanner = () => {
-  const [scanResult, setScanResult] = useState('');
-
-  const onScanSuccess = (e: { data: React.SetStateAction<string>; }) => {
-    setScanResult(e.data);
+export default class QRScanner extends Component {
+  onSuccess = (e: { data: string; }) => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
   };
-  //TODO: do sth with scanresult
-  return (
-    <View style={styles.container}>
+
+  render() {
+    return (
       <QRCodeScanner
-        onRead={onScanSuccess}
-        reactivate={true}
-        reactivateTimeout={5000}
-        showMarker={true}
-        markerStyle={styles.markerStyle}
-        cameraStyle={styles.cameraStyle}
+        onRead={this.onSuccess}
+        reactivateTimeout={3000}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Scan the QR-Code on the Pocket-Party host device!
+          </Text>
+        }
+        bottomContent={
+          <NumberInputField />
+        }
       />
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  centerText: {
+    fontFamily: 'Outfit',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 20,
+    textAlign: 'center',
+    color: '#53A57D'
   },
-  markerStyle: {
-    borderColor: '#FFF',
-    borderRadius: 10,
-  },
-  cameraStyle: {
-    height: '100%',
-  },
-});
 
-export default QRScanner;
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
