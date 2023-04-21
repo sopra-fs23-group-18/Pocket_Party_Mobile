4afa3729825c83ce0a14fc8611186c5233889f55
Nils Grob <nils.grob@protonmail.com>
2023-04-21 13:56:57 +0200

    made sonar cloud happy

--- util/getDomain.ts
+++ util/getDomain.ts
@@ -3,6 +3,6 @@ import { isProduction } from "./isProduction"
 
 export const getWsUrl = () => {
     const prodUrl = 'wss://my-server-url.oa.r.appspot.com/' // TODO: insert your prod url for server (once deployed)
-    const devUrl = Platform.OS === "android" ? 'ws://localhost:8080' : 'ws://localhost:8080';
+    const devUrl = 'ws://localhost:8080'
     return isProduction() ? prodUrl : devUrl
   }
